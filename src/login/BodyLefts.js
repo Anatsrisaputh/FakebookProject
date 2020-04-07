66467e8a576e0b95594b5bdc4d53aa8b5ef94643
Anatsrisaputh <jane.stbc@gmail.com>
2020-04-07 22:10:29 +0700

    Commit Fakebook

---
+++ src/login/BodyLefts.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BodyLeft extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
